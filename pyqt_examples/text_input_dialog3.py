26182bd1723dcce27375148d978b4349cf43367d
André Roberge <andre.roberge@gmail.com>
2014-12-13 14:38:27 -0400

    cleaning up

--- pyqt_examples/text_input_dialog3.py
+++ pyqt_examples/text_input_dialog3.py
@@ -1,14 +1,16 @@
 '''Simple text input dialog'''
 from PyQt4 import QtGui, QtCore
 
-def text_input(question="Enter your response", default="", font=None):
+def text_input(message="Enter your response", default="", font=None):
     app = QtGui.QApplication([])
     if font is not None:
         try:
             family, size = font
             default_font = QtGui.QFont()
-            default_font.setFamily(family)
-            default_font.setPointSize(size)
+            if family:
+                default_font.setFamily(family)
+            if size:
+                default_font.setPointSize(size)
             app.setFont(default_font)
         except:
             print("Can not set font. Expected font = (family:str, size:int).")
@@ -18,7 +20,7 @@ def text_input(question="Enter your response", default="", font=None):
     flags |= QtCore.Qt.FramelessWindowHint
 
     text, ok = QtGui.QInputDialog.getText(None, '',
-        question, QtGui.QLineEdit.Normal, default, flags)
+        message, QtGui.QLineEdit.Normal, default, flags)
     app.quit()
     if ok:
         return text
@@ -26,11 +28,15 @@ def text_input(question="Enter your response", default="", font=None):
         return None  # I know, not needed since it is the default...
 
 if __name__ == '__main__':
-    answer = text_input(question="What is your name?")
+    answer = text_input(message="What is your name?")
     print(answer)
     answer = text_input(default="response")
     print(answer)
-    answer = text_input(font=("Helvetica", 12))
+    answer = text_input(message="Test set font", font=("Times", 12))
     print(answer)
-    answer = text_input(font="Helvetica")
+    answer = text_input(message="Test set font size only", font=("", 12))
+    print(answer)
+    answer = text_input(message="Test set font family only", font=("Courier", 0))
+    print(answer)
+    answer = text_input(message="Intentional font error", font="Helvetica")
     print(answer)
