cb268d8d6b2c6c4c529dbe3bd1163acd02f8c349
André Roberge <andre.roberge@gmail.com>
2014-12-15 10:56:12 -0400

    multiple changes: cleaning code, adding new function, improving demo

--- experiment/bguic.py
+++ experiment/bguic.py
@@ -2,9 +2,9 @@
 """Basic Graphical User Interface Components
 """
 import os
-from collections import OrderedDict
-from functools import wraps
-from inspect import signature
+import collections
+import functools
+import inspect
 from PyQt4 import QtGui, QtCore
 
 CONFIG = {}
@@ -26,8 +26,8 @@ def with_app(func):
         kwargs['app'] = app
         try:
             response = func(*args, **kwargs)
-        except TypeError:
-            sig = signature(func)
+        except TypeError:   # perhaps 'app' was not a keyword argument for func
+            sig = inspect.signature(func)
             if 'app' in sig.parameters.values():
                 raise
             else:
@@ -35,12 +35,12 @@ def with_app(func):
                 response = func(*args, **kwargs)
         app.quit()
         return response
-    return wraps(func)(_decorator)
+    return functools.wraps(func)(_decorator)
 
 
 def find_qm_files():
     """looking for files with names == qt_locale.qm"""
-    all_files = OrderedDict()
+    all_files = collections.OrderedDict()
     for root, _, files in os.walk(os.path.join(QtGui.__file__, "..")):
         for fname in files:
             if (fname.endswith('.qm') and fname.startswith("qt_")
@@ -56,12 +56,10 @@ class SimpleApp(QtGui.QApplication):
        with added methods useful for working with dialogs
        that are not class based.
       """
-    def __init__(self, locale=None, font_size=None):
+    def __init__(self):
         super().__init__([])
-        if font_size is not None:
-            self.set_font_size(font_size)
         self.setFont(CONFIG['font'])
-        self.set_locale(locale)
+        self.set_locale(None)  # recover locale set by previous run, if any ...
 
     def set_locale(self, locale):
         """Sets the language of the basic controls for PyQt
@@ -83,40 +81,6 @@ class SimpleApp(QtGui.QApplication):
                                          QM_FILES[CONFIG['locale']]):
                 self.installTranslator(CONFIG['translator'])
 
-    @staticmethod
-    def set_font_size(font_size):
-        """Simple method to set font size; also called by individual GUI
-           components.
-        """
-        try:
-            CONFIG['font'].setPointSize(font_size)
-        except TypeError:
-            print("font_size must be an integer")
-
-    @staticmethod
-    def show_text_input(message="message", title="title",
-                        default_response=""):
-        """Obtain a response as a string from a user following a query.
-           In many ways, this is meant as a GUI replacement for
-           Python's input().
-        """
-        flags = QtCore.Qt.WindowSystemMenuHint | QtCore.Qt.WindowTitleHint
-        text, ok = QtGui.QInputDialog.getText(None, title, message,
-                               QtGui.QLineEdit.Normal, default_response, flags)
-        if ok:
-            return text
-
-
-    def show_select_language(self, title="Select language",
-                             name="Language codes",
-                             instruction="Click button when you are done"):
-        """Calls a special dialog showing locale for language (qm) files
-        that were found so that the language of the UI can be set.
-        """
-        selector = _LanguageSelector(self, title=title, name=name,
-                                     instruction=instruction)
-        selector.exec_()
-
 
 class _LanguageSelector(QtGui.QDialog):
     """A specially constructed dialog which uses informations about
@@ -132,16 +96,15 @@ class _LanguageSelector(QtGui.QDialog):
         self.qm_files_choices = {}
         self.parent = parent
 
+        # ========= check boxes ==============
         group_box = QtGui.QGroupBox(name)
         group_box_layout = QtGui.QGridLayout()
-
         for i, locale in enumerate(QM_FILES):
             check_box = QtGui.QCheckBox(locale)
             check_box.setAutoExclusive(True)
             self.qm_files_choices[check_box] = locale
             check_box.toggled.connect(self.check_box_toggled)
             group_box_layout.addWidget(check_box, i / 4, i % 4)
-
         # adding default language option. When using the PyQt distribution
         # no "en" files were found and yet "en" was the obvious default.
         # We need this option in case we want to revert a change.
@@ -151,14 +114,14 @@ class _LanguageSelector(QtGui.QDialog):
         check_box.toggled.connect(self.check_box_toggled)
         i = len(QM_FILES)
         group_box_layout.addWidget(check_box, i / 4, i % 4)
-
         group_box.setLayout(group_box_layout)
 
+        # ========= buttons ==============
         button_box = QtGui.QDialogButtonBox()
-
         confirm_button = button_box.addButton(QtGui.QDialogButtonBox.Ok)
         confirm_button.clicked.connect(self.confirm)
 
+        # ========= finalizing layout ====
         main_layout = QtGui.QVBoxLayout()
         main_layout.addWidget(group_box)
         main_layout.addWidget(QtGui.QLabel(instruction))
@@ -172,51 +135,44 @@ class _LanguageSelector(QtGui.QDialog):
 
     def confirm(self):
         """Callback from confirm_button used to set the locale"""
-        self.parent.set_locale(self.locale)
+        if self.locale != CONFIG['locale']:
+            self.parent.set_locale(self.locale)
         self.close()
 
 
-def set_global_font(app=None, locale=None):
+@with_app
+def set_global_font():
     """GUI component to set default font"""
-    if app is None:
-        app_quit = True
-        app = SimpleApp(locale=locale)
-    else:
-        app_quit = False
     font, ok = QtGui.QFontDialog.getFont(CONFIG['font'], None)
-    if app_quit:
-        app.quit()
     if ok:
         CONFIG['font'] = font
 
-
+@with_app
 def text_input(message="Enter your response", title="Title",
-               default_response="",
-               font_size: int=None,
-               locale: "most often two letter code such as 'fr'"=None):
+               default_response=""):
     """Simple text input box.  Used to query the user and get a string back.
     """
-    app = SimpleApp(font_size=font_size, locale=locale)
-    response = app.show_text_input(message=message, title=title,
-                                   default_response=default_response)
-    app.quit()
-    return response
+    flags = QtCore.Qt.WindowSystemMenuHint | QtCore.Qt.WindowTitleHint
+    text, ok = QtGui.QInputDialog.getText(None, title, message,
+                           QtGui.QLineEdit.Normal, default_response, flags)
+    if ok:
+        return text
 
 
 @with_app
-def yes_no_question(question="Answer this question", title="Title",
-                    font_size=None, locale=None):
+def yes_no_question(question="Answer this question", title="Title"):
     """Simple yes or no question; returns True for "Yes", False for "No"
        and None for "Cancel".
     """
-    reply = QtGui.QMessageBox.question(None, title, question,
-                    QtGui.QMessageBox.Yes
-                    | QtGui.QMessageBox.No
-                    | QtGui.QMessageBox.Cancel)
+    flags = QtGui.QMessageBox.Yes | QtGui.QMessageBox.No
+    flags |= QtGui.QMessageBox.Cancel
+
+    reply = QtGui.QMessageBox.question(None, title, question, flags)
     if reply == QtGui.QMessageBox.Yes:
         return True
     elif reply == QtGui.QMessageBox.No:
         return False
+
 
 @with_app
 def select_language(title="Select language", name="Language codes",
@@ -227,6 +183,11 @@ def select_language(title="Select language", name="Language codes",
     selector = _LanguageSelector(app, title=title, name=name,
                                      instruction=instruction)
     selector.exec_()
+
+@with_app
+def set_locale(locale, app=None):
+    '''Sets the locale, if available'''
+    app.set_locale(locale)
 
 
 @with_app
@@ -247,13 +208,24 @@ def integer_input(message="Choose a number", title="Title",
     """
     flags = QtCore.Qt.WindowSystemMenuHint | QtCore.Qt.WindowTitleHint
     number, ok = QtGui.QInputDialog.getInteger(None,
-                title, message, default_value, min_, max_, step, flags)
+                title, message,
+                default_value, min_, max_, step, flags)
     if ok:
         return number
+
+
+def set_font_size(font_size):
+    """Simple method to set font size.
+    """
+    try:
+        CONFIG['font'].setPointSize(font_size)
+    except TypeError:
+        print("font_size must be an integer")
 
 
 if __name__ == '__main__':
     try:
         import bguic_demo
+        bguic_demo.guessing_game()
     except ImportError:
         print("Could not find demo.")
--- experiment/bguic_demo.py
+++ experiment/bguic_demo.py
@@ -1,71 +1,55 @@
 import bguic
 from random import randint
 
-#print("Selecting a new ui language")
-#bguic.select_language()
-#
-#answer = bguic.yes_no_question("Make a wise choice")
-#print("\nAnswer to yes or no question: ", answer)
-#
-#answer = bguic.text_input(font_size=16)
-#print("\nResponse to text_input query:", answer)
-#
-#print("Selecting a new ui language")
-#bguic.select_language()
-#
-#answer = bguic.yes_no_question("Font too big?")
-#print("\nFont too big?: ", answer)
-#
-#if answer:
-#    print("\nSetting global font")
-#    bguic.set_global_font()
-#
+
+def guessing_game():
+    name = bguic.text_input(message="What is your name?",
+                            title="Mine is Reeborg.")
+    if not name:
+        name = "Unknown person"
+
+    message = """<p>The following language selection will only affect the
+        default GUI elements like the text on the buttons.
+        Note that <b>Default</b> is reverting back to the
+        local PyQt default (likely English).</p>"""
+
+    bguic.message_box(message=message, title="For information")
+    bguic.select_language()
+
+    bguic.message_box(message="If the text is too small or too large," +
+                      " you can fix that",
+                      title="For information")
+    bguic.set_global_font()
+    bguic.message_box(message="Hello {}. Let's play a game".format(name),
+                      title="Guessing game!")
+
+    guess = min_ = 1
+    max_ = 50
+    answer = randint(min_, max_)
+    title = "Guessing game"
+    while guess != answer:
+        message = "Guess a number between {} and {}".format(min_, max_)
+        prev_guess = guess
+        guess = bguic.integer_input(message=message, title=title,
+                              default_value=guess, min_=min_ ,max_=max_)
+        if guess is None:
+            quitting = bguic.yes_no_question("Do you want to quit?")
+            guess = prev_guess
+            if quitting:
+                break
+        elif guess < answer:
+            title = "Too low"
+            min_ = guess
+        elif guess > answer:
+            title = "Too high"
+            max_ = guess
+    else:
+        bguic.message_box(message="Congratulations {}! {} was the answer.".format(name, guess),
+                          title="You win!")
 
 
-
-####################################
-name = bguic.text_input(message="What is your name?",
-                        title="Mine is Reeborg.")
-if not name:
-    name = "Unknown person"
-
-
-bguic.message_box(message="The following language selection will only affect"+
-                  " the default GUI elements like the text on the buttons",
-                  title="For information")
-bguic.select_language()
-
-
-bguic.message_box(message="If the text is too small or too large, you can fix that",
-                  title="For information")
-bguic.set_global_font()
-
-bguic.message_box(message="Hello {}. Let's play a game".format(name),
-                  title="Guessing game!")
-
-guess = min_ = 1
-max_ = 50
-answer = randint(min_, max_)
-title = "Guessing game"
-while guess != answer:
-    message = "Guess a number between {} and {}".format(min_, max_)
-    prev_guess = guess
-    guess = bguic.integer_input(message=message, title=title,
-                          default_value=guess, min_=min_ ,max_=max_)
-    if guess is None:
-        quitting = bguic.yes_no_question("Do you want to quit?")
-        guess = prev_guess
-        if quitting:
-            break
-    elif guess < answer:
-        title = "Too low"
-        min_ = guess
-    elif guess > answer:
-        title = "Too high"
-        max_ = guess
-else:
-    bguic.message_box(message="Congratulations {}! {} was the answer.".format(name, guess),
-                      title="You win!")
-
-
+if __name__ == '__main__':
+    bguic.message_box("Temporarily setting the locale to Spanish")
+    bguic.set_locale('es')
+    guessing_game()
 
