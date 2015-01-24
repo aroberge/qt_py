c3fb131ca053eab9dc62b370a693009215e61b4c
André Roberge <andre.roberge@gmail.com>
2015-01-24 01:42:50 -0400

    smaller splash screen example

---
+++ varia/splash.py
@@ -0,0 +1,18 @@
+''' Small app: just a text splash screen '''
+
+import sys
+
+import PyQt4.QtCore as Core
+import PyQt4.QtGui as Gui
+
+def show_message(message):
+    '''shows a message as a splash screen'''
+    app = Gui.QApplication(sys.argv)
+    label = Gui.QLabel("<font color=blue size=20><b>" + message + "</b></font>")
+    label.setWindowFlags(Core.Qt.SplashScreen)
+    label.show()
+    Core.QTimer.singleShot(10000, app.quit)
+    app.exec_()
+
+if __name__ == '__main__':
+    show_message("Hello world!")
